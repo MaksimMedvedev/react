813e702afe799b9bb130bbddd7412bfbe08c0ffc
Maksim Medvedev <own77d@gmail.com>
2019-06-26 12:09:53 +0500

    Edit task 2.1 code (index.js)

--- tasks/src/2.1.ExtractFunction/index.js
+++ tasks/src/2.1.ExtractFunction/index.js
@@ -6,33 +6,39 @@ import './styles.css';
      Выдели метод отрисовки лота (renderLot), метод отрисовки поста (renderPost) и используй их.
  */
 
+function lot(lotName, lotDescription) {
+  return (<div className="lot">
+    <div className="lotName">{lotName}</div>
+    <div className="lotDescription">{lotDescription}</div>
+  </div>);
+}
+
+function post(postAuthor, postTime, postMessage) {
+  return (<div className="post">
+    <div className="postHeader">
+      <span className="postAuthor">{postAuthor}</span>
+      <br />
+      <span className="postTime">{postTime}</span>
+    </div>
+    <div className="postMessage">{postMessage})</div>
+  </div>);
+}
+
 ReactDom.render(
   <div className="page">
-    <div className="lot">
-      <div className="lotName">Форма для выпекания</div>
-      <div className="lotDescription">Идеальна для приготовления десертов!</div>
-    </div>
+    {
+      lot('Форма для выпекания', 'Идеальна для приготовления десертов!')
+    }
     <div className="posts">
-      <div className="post">
-        <div className="postHeader">
-          <span className="postAuthor">Парень не промах</span>
-          <br />
-          <span className="postTime">2 часа назад</span>
-        </div>
-        <div className="postMessage">Попробую с удовольствием ;)</div>
-      </div>
-      <div className="post">
-        <div className="postHeader">
-          <span className="postAuthor">Милая девушка</span>
-          <br />
-          <span className="postTime">3 часа назад</span>
-        </div>
-        <div className="postMessage">
-          Можно использовать для выпекания чизкейков :)
-        </div>
-      </div>
+      {
+        post('Парень не промах', '2 часа назад', 'Попробую с удовольствием')
+      }
+      {
+        post('Милая девушка', '3 часа назад', 'Можно использовать для выпекания чизкейков :)')
+      }
     </div>
   </div>,
+
   document.getElementById('app')
 );
 
